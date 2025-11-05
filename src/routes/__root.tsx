0984a63019faf899cfbb0cbbf6b2e23f07809aff
arifinoid <rohmadarifin007@gmail.com>
2025-11-05 17:04:20 +0000

    chore: tidy up

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,4 +1,4 @@
-import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 export const Route = createRootRoute({
@@ -8,11 +8,6 @@ export const Route = createRootRoute({
 function RouteComponent() {
   return (
     <>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-      </div>
-      <hr />
       <Outlet />
       <TanStackRouterDevtools />
     </>
